74e0d992efdd8cd01aa680abcb046d89eea1ece3
AliKakoo <alikakuo@yahoo.com>
2025-02-01 21:01:28 +0330

    add codes for select element and add names

---
+++ script.js
@@ -0,0 +1,75 @@
+const $ = document
+
+const wrapperElem = $.querySelector('.wrapper')
+const selectBtn = $.querySelector(".select-btn");
+const options = $.querySelector('.options')
+const searchInputElem = $.querySelector('input')
+
+let countries = [
+  "Afghanistan",
+  "Algeria",
+  "Argentina",
+  "Australia",
+  "Bangladesh",
+  "Belgium",
+  "Bhutan",
+  "Brazil",
+  "Canada",
+  "China",
+  "Denmark",
+  "Ethiopia",
+  "Finland",
+  "France",
+  "Germany",
+  "Hungary",
+  "Iceland",
+  "India",
+  "Indonesia",
+  "Iran",
+  "Italy",
+  "Japan",
+  "Malaysia",
+  "Maldives",
+  "Mexico",
+  "Morocco",
+  "Nepal",
+  "Netherlands",
+  "Nigeria",
+  "Norway",
+  "Pakistan",
+  "Peru",
+  "Russia",
+  "Romania",
+  "South Africa",
+  "Spain",
+  "Sri Lanka",
+  "Sweden",
+  "Switzerland",
+  "Thailand",
+  "Turkey",
+  "Uganda",
+  "Ukraine",
+  "United States",
+  "United Kingdom",
+  "Vietnam",
+];
+
+function addCountrires (){
+    let li = null;
+    countries.forEach(country => {
+        li = `<li onclick = "updateName(this)">${country}</li>`;
+        options.insertAdjacentHTML('beforeend', li)
+    });
+
+}
+
+function updateName (el){
+ console.log(el.textContent)
+}
+
+
+selectBtn.addEventListener('click', ()=>{
+    wrapperElem.classList.toggle('active');
+});
+
+window.addEventListener('load', addCountrires)
