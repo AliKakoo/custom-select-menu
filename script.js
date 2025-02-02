351f88eba0f120b9aacccbd15dbd1f4b30b5de20
AliKakoo <alikakuo@yahoo.com>
2025-02-02 21:29:12 +0330

    add new feature for add names selected to box select name country

--- script.js
+++ script.js
@@ -63,8 +63,25 @@ function addCountrires (){
 
 }
 
+// add country name to box select name
+
 function updateName (el){
- console.log(el.textContent)
+ wrapperElem.classList.remove("active");
+
+ // if type country names in search box that be null
+  searchInputElem.value = ''
+
+
+ // for add class css selected to country names select
+ for (let option of options.children){
+    if (option.innerText === el.innerText) {
+        option.classList.add('selected')
+    } else {
+        option.classList.remove("selected");
+        
+    }
+ }
+ selectBtn.firstElementChild.innerText = el.innerText;
 }
 
 
